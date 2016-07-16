b19e3970faaf1d4e35291f6330b208f6944bbe4e
John.Koepi <john.koepi@gmail.com>
2016-07-16 22:29:02 +0200

    fixed clang < 3.8 debug mode 64 bit optimization failure under valgrind memcheck

--- src/game/food.h
+++ src/game/food.h
@@ -10,6 +10,14 @@ struct Food {
 
   uint8_t size;
   uint8_t color;  // at least 28 types of colors
+
+  // some compilers would not auto pad size of this struct,
+  // and valgrind will blow this up when reading from 64 bit reg
+  uint16_t _padding = 0;
+
+  Food() = default;
+  Food(uint16_t in_x, uint16_t in_y, uint8_t in_size, uint8_t in_color)
+      : x(in_x), y(in_y), size(in_size), color(in_color) { }
 };
 
 typedef std::vector<Food> FoodSeq;
